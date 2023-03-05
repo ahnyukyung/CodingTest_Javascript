f0e8ff93efef6d852292a7619a55b3bffd7ab51b
AHNYUKYUNG <81419265+ahnyukyung@users.noreply.github.com>
2023-03-05 09:44:06 +0900

    [level 0] Title: 각도기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    let=0;
+ if(angle<90){
+     return 1
+ }else if(angle == 90){
+     return 2
+ }else if(angle < 180){
+     return 3
+ }else if(angle == 180){
+     return 4
+ }
+// return [0, 90, 91, 180].filter(x => angle>=x).length;
+}
