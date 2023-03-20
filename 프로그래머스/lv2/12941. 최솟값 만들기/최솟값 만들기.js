40005a5ae2dcdec80989d40ea9cdea192105b5f6
AHNYUKYUNG <81419265+ahnyukyung@users.noreply.github.com>
2023-03-20 19:04:17 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.72 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -0,0 +1,13 @@
+function solution(A, B) {
+        let answer = 0;
+        A.sort(function (a, b) {
+          return a - b;
+        });
+        B.sort(function (a, b) {
+          return b - a;
+        });
+        for (let i = 0; i < A.length; i++) {
+          answer += A[i] * B[i];
+        }
+        return answer;
+      }
